92c247d0e8dc78ee550498b80fa163a5e9c18ef0
Moises Cuevas <moises.cuevas@autofact.cl>
2022-06-12 15:23:56 -0400

    class active menu

--- resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/sidebar.blade.php
@@ -1,3 +1,5 @@
+@inject('request', 'Illuminate\Http\Request')
+
 <!-- Left side column. contains the logo and sidebar -->
 <aside class="main-sidebar">
 
@@ -8,7 +10,7 @@
         @if (! Auth::guest())
             <div class="user-panel">
                 <div class="pull-left image">
-                    <img src="{{ Gravatar::get($user->email) }}" class="img-circle" alt="User Image" />
+                    <img src="{{ Gravatar::get($user->email) }}" class="img-circle" alt="{{ Auth::user()->name }}" />
                 </div>
                 <div class="pull-left info">
                     <p style="overflow: hidden;text-overflow: ellipsis;max-width: 160px;" data-toggle="tooltip" title="{{ Auth::user()->name }}">{{ Auth::user()->name }}</p>
@@ -20,8 +22,9 @@
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">Menu Principal</li>
             <!-- Optionally, you can add icons to the links -->
-            <li class="active"><a href="{{ url('home') }}"><i class='fa fa-home'></i> <span>Home</span></a></li>
-            <li><a href="{{ url('/productos') }}"><i class='fa fa-shopping-basket'></i> <span>Productos</span></a></li>
+            <li class="{{ ($request->segment(1) == '')?'active':'' }}"><a href="{{ url('home') }}"><i class='fa fa-home'></i> <span>Home</span></a></li>
+            <li class="{{ ($request->segment(1) == 'productos')?'active':'' }}"><a href="{{ url('/productos') }}"><i class='fa fa-shopping-basket'></i> <span>Productos</span></a></li>
+            <li class="{{ ($request->segment(1) == 'categorias')?'active':'' }}"><a href="{{ url('/categorias') }}"><i class='fa fa-shopping-basket'></i> <span>Categorias</span></a></li>
         </ul>
     </section>
     <!-- /.sidebar -->
